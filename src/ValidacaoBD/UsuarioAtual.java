7b6a7d5fd0395ce1a9997ca8c564f4a1f6cc6c15
João Victor <joaosaiko14@gmail.com>
2024-02-01 22:26:38 -0300

    Criação da classe 'UsuarioAtual'. Pequenas modificações na classe 'ValidacaoUser'. Criação da pasta 'jcalendar' para o arquivo 'jcalendar-1.4.jar'

---
+++ src/ValidacaoBD/UsuarioAtual.java
@@ -0,0 +1,5 @@
+package ValidacaoBD;
+
+public class UsuarioAtual {
+	//será responvel por pegar o id do usuario atual para cada tarefa ser adicionada com base no id do usuario logado.
+}
--- src/ValidacaoBD/ValidacaoUser.java
+++ src/ValidacaoBD/ValidacaoUser.java
@@ -1,10 +1,8 @@
 package ValidacaoBD;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import ConexaoBD.ConexaoBD;
 
 public class ValidacaoUser {
